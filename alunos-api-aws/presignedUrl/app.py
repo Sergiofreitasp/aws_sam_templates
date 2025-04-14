e7b8afdee3c71e980380191586f647d43b50c53f
Sérgio <55296032+Sergiofreitasp@users.noreply.github.com>
2025-04-14 15:22:18 -0300

    adicionando headers no app.py do presignedUrl

--- alunos-api-aws/presignedUrl/app.py
+++ alunos-api-aws/presignedUrl/app.py
@@ -43,6 +43,10 @@ def lambda_handler(event, context):
     return {
 
         "statusCode": 200,
+        'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+        },
         "body": json.dumps({
             "message": "Url pré assinada criada com sucesso",
             "url": presigned_url,
